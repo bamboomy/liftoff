d00c11c6f0bd3d0e02efcdfd0137a684182390ad
Theetaert <bamboomy@gmail.com>
2016-08-25 19:46:47 +0200

    to be reviewed app is shown

--- dev/php/crib.php
+++ dev/php/crib.php
@@ -102,7 +102,7 @@ if(!isset($_SESSION['login_user'])){
 					<div class="row">
 						<div class="col-sm-1"></div>
 						<div class="col-sm-10">
-							<h4>Written</h4>
+							<h4>Written:</h4>
 						</div>
 						<div class="col-sm-1"></div>
 					</div>
@@ -110,7 +110,7 @@ if(!isset($_SESSION['login_user'])){
 					<div class="row">
 						<div class="col-sm-1"></div>
 						<div class="col-sm-10">
-							<h4>Recieved</h4>
+							<h4>Recieved:</h4>
 						</div>
 						<div class="col-sm-1"></div>
 					</div>
@@ -140,10 +140,29 @@ if($result->num_rows > 0){
 						<div class="col-sm-10">
 							<h4>On the to be reviewed list:</h4>
 <?	
-/*	
-	while(){
+
+	while($row = $result->fetch_assoc()) {
+?>		
+							<div class="row">
+								<div class="col-sm-2">
+									<? echo "<img src='".$row["src"]."' width='100' height='100'/>"; ?>
+								</div>
+								<div class="col-sm-8">
+									<div class="row">
+										<div class="col-sm-12">
+											<h4><? echo $row["title"]; ?></h4>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-sm-12">
+											<h4><? echo "position: "; ?> </h4>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-2"></div>
+							</div>
+<?		
 	}
-*/
 ?>
 						</div>
 						<div class="col-sm-1"></div>
